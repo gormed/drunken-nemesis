a3fa55fb3fa6cbc087bbd0f7eded20a4498afcde
gormed <hans.ferchland@gmx.de>
2013-06-20 01:26:15 +0200

    fix

--- opus/OpusProject/src/com/opus/gui/frames/NewsFrameMenu.java
+++ opus/OpusProject/src/com/opus/gui/frames/NewsFrameMenu.java
@@ -55,7 +55,7 @@ public class NewsFrameMenu extends AbstractFrameMenu {
         createBackground(0.333f, new Color(203, 75, 59));
         createBackground(0.666f, new Color(203, 75, 59));
 
-   
+    }
     
     private BitmapText createRotated(AbstractFrameMenuEntry menuentry, float angle, String title) {
         Text entry = new Text(false);
