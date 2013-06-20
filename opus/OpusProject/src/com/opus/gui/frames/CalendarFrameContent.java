d120944ad661e747c0b77da8cc6b393a67f09258
Julian Sakowski <Senju@hotmail.de>
2013-06-20 15:09:54 +0200

    added day name

--- opus/OpusProject/src/com/opus/gui/frames/CalendarFrameContent.java
+++ opus/OpusProject/src/com/opus/gui/frames/CalendarFrameContent.java
@@ -20,8 +20,11 @@ import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Property;
 import com.opus.logic.Properti;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 
 /**
  *
@@ -30,9 +33,9 @@ import java.util.Comparator;
 public class CalendarFrameContent extends AbstractFrameContent {
 
     private int userID = -1;
+    private int actualDate = Integer.parseInt(getDate());
     private int dayID = 0;
     private int dayComponentNumber = 0;
-    
     private ArrayList<String> propertiesDTSTART = new ArrayList<String>(0);
     private ArrayList<String> propertiesEND = new ArrayList<String>(0);
     private ArrayList<String> propertiesSUMMARY = new ArrayList<String>(0);
@@ -61,41 +64,45 @@ public class CalendarFrameContent extends AbstractFrameContent {
         sortProperties();
         /*testOutput();*/
         //QuickSort.getInstance().sortArray(propertiesDTSTART, 0, propertiesDTSTART.size()-1);
-        
+
         //Initialisieren wie viele Termine es an diesem Tag gibt
         String day = properties.get(dayID).getDTSTART();
         day = day.substring(0, 8);
-        for(Properti prop : properties){
-          String date = prop.getDTSTART();
-          date = date.substring(0,8);
-            if(day.equals(date)){
-              dayComponentNumber++; 
-          }             
+        for (Properti prop : properties) {
+            String date = prop.getDTSTART();
+            date = date.substring(0, 8);
+            if (day.equals(date)) {
+                dayComponentNumber++;
+            }
         }
-        
+
         //Die Termine in einen String verknüpfen
         String input = "";
-        for(int i = 0; i < dayComponentNumber; i++){
-          String start = properties.get(dayID + i).getDTSTART();
-          start = start.substring(9,11) + ":" + start.substring(11,13);
-          String end = properties.get(dayID + i).getDTEND();
-          end = end.substring(9,11) + ":" + end.substring(11,13);;
-          input = input + properties.get(dayID + i).getSUMMARY() + ":  " + start + " - " + end + " - " + properties.get(dayID + i).getLOCATION() + "\n";  
+        for (int i = 0; i < dayComponentNumber; i++) {
+            String start = properties.get(dayID + i).getDTSTART();
+            start = start.substring(9, 11) + ":" + start.substring(11, 13);
+            String end = properties.get(dayID + i).getDTEND();
+            end = end.substring(9, 11) + ":" + end.substring(11, 13);;
+            input = input + properties.get(dayID + i).getSUMMARY() + ":  " + start + " - " + end + " - " + properties.get(dayID + i).getLOCATION() + "\n";
         }
-        
-        Heading h1 = new Heading(false,  new ColorRGBA(42f/255f, 101f/255f, 137f/255f,1f));
-        h1.setText(day);
-        h1.setLocalTranslation(-h1.getLineWidth()*0.5f, 100, 0);
+
+        Heading h1 = new Heading(false, new ColorRGBA(42f / 255f, 101f / 255f, 137f / 255f, 1f));
+        try{
+            h1.setText(getDay(day));
+        } catch (Exception e){
+            System.out.println("CalendarFrameContent - createContent: " + e);
+        }
+        h1.setLocalTranslation(-h1.getLineWidth() * 0.5f, 100, 0);
         attachChild(h1);
-        Text message = new Text(false, new ColorRGBA(42f/255f, 101f/255f, 137f/255f,1f));
+        Text message = new Text(false, new ColorRGBA(42f / 255f, 101f / 255f, 137f / 255f, 1f));
         message.setBox(new Rectangle(-100, 60, 200, 200));
         System.out.println(input);
         message.setText(input);
-       // message.setLocalTranslation(-message.getLineWidth()*0.5f, -message.getLineHeight()*message.getLineCount()*0.5f, 0);
+        // message.setLocalTranslation(-message.getLineWidth()*0.5f, -message.getLineHeight()*message.getLineCount()*0.5f, 0);
         attachChild(message);
-        
-                
-        
+
+
+
         dayComponentNumber = 0;
         input = "";
     }
@@ -120,7 +127,7 @@ public class CalendarFrameContent extends AbstractFrameContent {
                     propertiesLOCATION.add(property.getValue());
                     //System.out.println("add-LOCATION: " + property.getName() + " " + property.getValue());
                 } else {
-                    System.out.println("CalendarFrameContent - init Content: Properti " + property.getName() + " " + property.getValue());
+                    //System.out.println("CalendarFrameContent - init Content: Properti " + property.getName() + " " + property.getValue());
                 }
             }
         }
@@ -155,15 +162,23 @@ public class CalendarFrameContent extends AbstractFrameContent {
     }
 
     private void testOutput() {
-        for(Properti pro : properties){
+        for (Properti pro : properties) {
             System.out.println(pro.getDTSTART());
         }
     }
-    /*
-    private String getDate() { 
-        SimpleDateFormat formatter = new SimpleDateFormat( 
-                "yyyy.MM.dd - HH:mm:ss "); 
-        Date currentTime = new Date(); 
-        System.out.println(formatter.format(currentTime));        // 2012.04.14 - 21:34:07  
-    }*/
+
+    private String getDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        Date currentDate = new Date();
+        System.out.println("get DATE: " + formatter.format(currentDate));
+        return formatter.format(currentDate);
+    }
+
+    private String getDay(String date) throws ParseException {
+        SimpleDateFormat dayFormat = new SimpleDateFormat( "EEEE" );
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+        String inputDate = date;
+        System.out.println(inputDate + " was a " + dayFormat.format(format.parse(inputDate)));
+        return dayFormat.format(format.parse(inputDate));
+    }
 }
